1e24d3191b1cb88466b9a9973de892d559da936c
LeoFagundo <leo.fagundo@kellogg.northwestern.edu>
2020-02-24 03:25:11 +0000

    week 7

--- dice.rb
+++ dice.rb
@@ -4,5 +4,24 @@ def view(template); erb template.to_sym; end
 before { puts "Parameters: #{params}" }  
 
 get "/" do
-  "Hello"
+    # ruby code...
+    # @die1 = rand(1..6)
+    # @die2 = rand(1..6)
+    @dice = []
+    for die in (0..4)
+        die = rand(1..6)
+        @dice << die
+    end
+
+
+
+
+
+#   "<h1>Dice: #{die1}, #{die2}</h1>"
+
+    view "dice"
+end
+
+get "/tacos" do
+    "tacos mmm"
 end
--- weather.rb
+++ weather.rb
@@ -1,24 +1,53 @@
 require "forecast_io"
 
 # configure the Dark Sky API with your API key
-ForecastIO.api_key = "YOUR-API-KEY"
+ForecastIO.api_key = "11aeb56853dd3a8b89a9f7acd90f6f07"
 
 # do the heavy lifting, use Global Hub lat/long
 forecast = ForecastIO.forecast(42.0574063,-87.6722787).to_hash
 
-# pp = pretty print
-# use instead of `puts` to make reading a hash a lot easier
-# e.g. `pp forecast`
+current_temp = forecast["currently"]["temperature"]
+current_summary = forecast["currently"]["summary"]
 
-# Example output:
-#
-# In Chicago, it is currently 33.89 degress and Clear
-# Extended forecast:
-# A high temperature of 36.47 and Possible flurries in the afternoon.
-# A high temperature of 33.98 and Mostly cloudy throughout the day.
-# A high temperature of 33.94 and Possible light snow until evening.
-# A high temperature of 32.73 and Mostly cloudy throughout the day.
-# A high temperature of 32.51 and Partly cloudy throughout the day.
-# A high temperature of 40.42 and Clear throughout the day.
-# A high temperature of 23.21 and Overcast throughout the day.
-# A high temperature of 29.12 and Clear throughout the day.
+day1_temp = forecast["daily"]["data"][0]["temperatureHigh"]
+day1_summary = forecast["daily"]["data"][0]["summary"]
+
+day2_temp = forecast["daily"]["data"][1]["temperatureHigh"]
+day2_summary = forecast["daily"]["data"][1]["summary"]
+
+day3_temp = forecast["daily"]["data"][2]["temperatureHigh"]
+day3_summary = forecast["daily"]["data"][2]["summary"]
+
+day4_temp = forecast["daily"]["data"][3]["temperatureHigh"]
+day4_summary = forecast["daily"]["data"][3]["summary"]
+
+day5_temp = forecast["daily"]["data"][4]["temperatureHigh"]
+day5_summary = forecast["daily"]["data"][4]["summary"]
+
+day6_temp = forecast["daily"]["data"][5]["temperatureHigh"]
+day6_summary = forecast["daily"]["data"][5]["summary"]
+
+day7_temp = forecast["daily"]["data"][6]["temperatureHigh"]
+day7_summary = forecast["daily"]["data"][6]["summary"]
+
+day8_temp = forecast["daily"]["data"][7]["temperatureHigh"]
+day8_summary = forecast["daily"]["data"][7]["summary"]
+
+# puts forecast["daily"]["data"][0]
+
+
+puts "In Chicago, it is currently #{current_temp} degress and #{current_summary}"
+puts "Extended forecast:"
+# puts "A high temperature of #{day1_temp} and #{day1_summary}"
+# puts "A high temperature of #{day2_temp} and #{day2_summary}"
+# puts "A high temperature of #{day3_temp} and #{day3_summary}"
+# puts "A high temperature of #{day4_temp} and #{day4_summary}"
+# puts "A high temperature of #{day5_temp} and #{day5_summary}"
+# puts "A high temperature of #{day6_temp} and #{day6_summary}"
+# puts "A high temperature of #{day7_temp} and #{day7_summary}"
+# puts "A high temperature of #{day8_temp} and #{day8_summary}"
+
+forecast_array = forecast["daily"]["data"]
+for weather_for_that_day in forecast_array
+    puts "A high temperature of #{weather_for_that_day["temperatureHigh"]} and #{weather_for_that_day["summary"]}"
+end
